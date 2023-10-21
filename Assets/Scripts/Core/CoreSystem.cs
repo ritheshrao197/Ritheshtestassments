3bb67b0cad8624c818dbfbf479dfa2d59facf8d0
Rithesh <rithesh197rao@gmail.com>
2023-10-22 03:49:43 +0530

    Added core system

---
+++ Assets/Scripts/Core/CoreSystem.cs
@@ -0,0 +1,63 @@
+﻿namespace MatchGame.Core
+{
+    public class CoreSystem
+    {
+        /// <summary>
+        /// Constructor for the CoreSystem class.
+        /// </summary>
+        public CoreSystem()
+        {
+            // Initialize the core system.
+            Init();
+
+            // Add event listeners or other setup operations.
+            AddListener();
+        }
+
+        /// <summary>
+        /// Initialize the core system.
+        /// </summary>
+        public virtual void Init()
+        {
+            // Implement initialization logic here.
+        }
+
+        /// <summary>
+        /// Add event listeners or perform other setup tasks.
+        /// </summary>
+        public virtual void AddListener()
+        {
+            // Implement event listener registration or other setup logic here.
+        }
+
+        /// <summary>
+        /// Remove event listeners or perform cleanup tasks.
+        /// </summary>
+        public virtual void RemoveListener()
+        {
+            // Implement event listener removal or cleanup logic here.
+        }
+
+        /// <summary>
+        /// Finalize and clean up resources.
+        /// </summary>
+        public virtual void Finalise()
+        {
+            // Implement resource cleanup or finalization logic here.
+        }
+
+        /// <summary>
+        /// Destructor (finalizer) for the CoreSystem class.
+        /// </summary>
+        ~CoreSystem()
+        {
+            // Finalize and clean up resources before destruction.
+            Finalise();
+
+            // Remove event listeners or perform additional cleanup.
+            RemoveListener();
+        }
+    }
+
+
+}
