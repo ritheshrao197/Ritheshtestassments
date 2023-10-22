5fc7a5ffefdde25d1eda338494e7ea3217503e9d
Rithesh <rithesh197rao@gmail.com>
2023-10-22 15:07:36 +0530

    main menu and gameplay ui implemeted with basic features

--- Assets/Scripts/Views/GamePlayView.cs
+++ Assets/Scripts/Views/GamePlayView.cs
@@ -1,28 +1,75 @@
 ﻿
 using System;
+using MatchGame.Core;
+using MatchGame.Data;
+using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace MatchGame.View
 {
     public class GamePlayView : View
     {
-        private Button _play;
+     [SerializeField]   private Button _pause;
+        GameDataContainer _gameDataContainer;
+        public CanvasGroup _screen;
 
         public override void Init()
         {
             base.Init();
-            _play.GetComponent<Button>();
-            _play.onClick.AddListener(() => OnPlayButtonClick());
+            _screen = GetComponent<CanvasGroup>();  // Get the CanvasGroup component from the GameObject.
+
+            _gameDataContainer = ServiceLocator.Instance.Get<GameDataContainer>();
+
+            _pause.onClick.AddListener(() => OnPause());
+            eventHandlerSystem.AddListener(GameEventKeys.GameStateUpdated, ShowGamePlay);
         }
 
-        private void OnPlayButtonClick()
+        private void OnPause()
         {
-            throw new NotImplementedException();
+
+            _gameDataContainer.GameState = State.Pause;
         }
         public override void Finalise()
         {
             base.Finalise();
-            _play.onClick.RemoveAllListeners();
+            _pause.onClick.RemoveAllListeners();
+            eventHandlerSystem.RemoveListener(GameEventKeys.GameStateUpdated, ShowGamePlay);
+
+        }
+
+        private void ShowGamePlay()
+        {
+            if (_gameDataContainer.GameState == State.Play)
+            {
+                EnableScreen ();
+            }
+            else if (_gameDataContainer.GameState == State.MainMenu)
+
+            {
+                DisableScreen();
+            }
+        }
+
+        /// <summary>
+        /// Enable the screen by making it interactable and visible.
+        /// </summary>
+        public void EnableScreen()
+        {
+            _screen.interactable = true;
+            _screen.blocksRaycasts = true;
+            _screen.alpha = 1;
+        }
+
+        /// <summary>
+        /// Disable the screen by making it non-interactable and invisible.
+        /// </summary>
+        public void DisableScreen()
+        {
+            // Deactivate the current screen.
+            _screen.interactable = false;
+            _screen.blocksRaycasts = false;
+            _screen.alpha = 0;
         }
     }
 }
--- Assets/Scripts/Views/MainMenuView.cs
+++ Assets/Scripts/Views/MainMenuView.cs
@@ -1,5 +1,8 @@
 ﻿
 using System;
+using MatchGame.Core;
+using MatchGame.Data;
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
@@ -7,23 +10,69 @@ namespace MatchGame.View
 {
     public class MainMenuView : View
     {
-      private  Button _play;
+        [SerializeField] private Button _play;
+        [SerializeField] private Button _quit;
+        GameDataContainer _gameDataContainer;
+        public CanvasGroup _screen;
 
         public override void Init()
         {
             base.Init();
-            _play.GetComponent<Button>();
-            _play.onClick.AddListener(()=>OnPlayButtonClick()); 
+            //_play.GetComponent<Button>();
+            _screen = GetComponent<CanvasGroup>();  // Get the CanvasGroup component from the GameObject.
+
+            _gameDataContainer = ServiceLocator.Instance.Get<GameDataContainer>();
+
+            _play.onClick.AddListener(()=>OnPlayButtonClick());
+            eventHandlerSystem.AddListener(GameEventKeys.GameStateUpdated, ShowMenu);
         }
 
         private void OnPlayButtonClick()
         {
-            throw new NotImplementedException();
+
+            _gameDataContainer.CurrentLevel = 2;
+            _gameDataContainer.GameState = State.Play;
         }
         public override void Finalise()
         {
             base.Finalise();
             _play.onClick.RemoveAllListeners();
+            eventHandlerSystem.RemoveListener(GameEventKeys.GameStateUpdated, ShowMenu);
+
+        }
+
+        private void ShowMenu()
+        {
+           if( _gameDataContainer.GameState == State.Play)
+            {
+                DisableScreen();
+            }
+            else if (_gameDataContainer.GameState == State.MainMenu)
+
+            {
+                EnableScreen();
+            }
+        }
+
+        /// <summary>
+        /// Enable the screen by making it interactable and visible.
+        /// </summary>
+        public void EnableScreen()
+        {
+            _screen.interactable = true;
+            _screen.blocksRaycasts = true;
+            _screen.alpha = 1;
+        }
+
+        /// <summary>
+        /// Disable the screen by making it non-interactable and invisible.
+        /// </summary>
+        public void DisableScreen()
+        {
+            // Deactivate the current screen.
+            _screen.interactable = false;
+            _screen.blocksRaycasts = false;
+            _screen.alpha = 0;
         }
     }
 }
