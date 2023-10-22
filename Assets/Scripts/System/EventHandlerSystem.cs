e02cff2cd9148b34d0f74e2f114ceb46d4b7ae72
Rithesh <rithesh197rao@gmail.com>
2023-10-22 06:32:33 +0530

    event handler and view system added

---
+++ Assets/Scripts/System/EventHandlerSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MatchGame.Core
+{
+    public class EventHandlerSystem :CoreSystem
+    {
+        private Dictionary<int, Action> eventDictionary = new Dictionary<int, Action>();
+
+        public void AddListener(int eventName, Action eventAction)
+        {
+            if (!eventDictionary.ContainsKey(eventName))
+            {
+                eventDictionary[eventName] = eventAction;
+            }
+            else
+            {
+                eventDictionary[eventName] += eventAction;
+            }
+        }
+
+        public void RemoveListener(int eventName, Action eventAction)
+        {
+            if (eventDictionary.ContainsKey(eventName))
+            {
+                eventDictionary[eventName] -= eventAction;
+
+                if (eventDictionary[eventName] == null)
+                {
+                    eventDictionary.Remove(eventName);
+                }
+            }
+        }
+        public void TriggerEvent(int eventName)
+        {
+            if (eventDictionary.TryGetValue(eventName, out Action eventAction))
+            {
+                eventAction.Invoke();
+            }
+        }
+    }
+
+}
